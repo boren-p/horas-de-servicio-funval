66732a726d4acd584c87984b8cf67179203f2d88
Borenp <borenkpardo@gmail.com>
2025-12-04 15:32:03 -0500

    cambios en header

--- src/comps/Header.jsx
+++ src/comps/Header.jsx
@@ -7,26 +7,25 @@ const Header = () => {
   const menuStudent = useRef(null);
 
   return (
-    <header className="bg-[#173B63]  h-25  flex justify-between items-center">
-      <nav className="flex text-3xl text-white h-full  items-center justify-center gap-4">
-        <Link to="/student">
-          <img src="/todo.png" alt="Funval internacional" className="h-20 " />
-        </Link>
-        <Link to="/student">
-          <nav>Home</nav>
-        </Link>
-      </nav>
+    <header className="bg-[#173B63]  h-25 px-[5%] flex justify-between items-center">
+      
 
-      <nav className="relative pr-5 " ref={menuStudent}>
+      <nav className="relative flex items-center justify-between w-screen" ref={menuStudent}>
+        <div className="flex items-center justify-between gap-10">
+        <Link to="/student">
+          <img src="/todo.png" alt="Funval internacional" className="h-15 " />
+        </Link>
+        <Link to="/student">
+          <nav className="hidden text-2xl text-white md:flex">Home</nav>
+        </Link>
+        </div>
+
         <img
-          className=" cursor-pointer"
-          width="50"
-          height="50"
+          className="h-15 w-auto cursor-pointer"
           src="https://img.icons8.com/pastel-glyph/64/FFFFFF/user-male-circle.png"
           alt="user-male-circle"
           onClick={() => setOpen(!open)}
         />
-
         {open && (
           <div className="absolute text-black right-0 mt-2 w-48 bg-white  rounded-lg shadow-lg z-10">
             <Link to="/edit">
